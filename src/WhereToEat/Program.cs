8eafbe05891e9e1b6863cfb9f830f1a58ba2437a
Thomas Manson <35357719+ThomasManson@users.noreply.github.com>
2023-05-22 12:32:46 +1000

    Update Program.cs

--- src/WhereToEat/Program.cs
+++ src/WhereToEat/Program.cs
@@ -3,6 +3,9 @@ namespace WhereToEat
     // test 3riI71BXed5ebIOYwQntykeZuhjsdweEc9BxH5Jc26w= 
     //3riI71BXed5ebIOYwQntykeZuhjsdweEc9BxH5Jc26w=
     // 3riI71BXed5ebIOYwQntykeZuhjsdweEc9BxH5Jc26w=
+    
+    // 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
+    
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
 
@@ -22,4 +25,7 @@ namespace WhereToEat
 });
         }
     }
+    
+    // 11A35CWLY0pGB8mZBOg9nZ_fErauuH77bLXRtkllwPMAbTUAV00lpqfIXsDkwBBqS1HVGFQH3Bhsgy0dok
+    
 }
